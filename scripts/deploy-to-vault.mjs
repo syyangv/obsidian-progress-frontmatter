5fb5be29a79a2783672226e03c68290247ff6fe4
syyangv <syyangv@gmail.com>
2026-07-13 22:47:44 -0400

    chore: update Obsidian vault path

--- scripts/deploy-to-vault.mjs
+++ scripts/deploy-to-vault.mjs
@@ -2,7 +2,7 @@ import { copyFile, mkdir, readFile, writeFile } from "node:fs/promises";
 import { join } from "node:path";
 
 const DEFAULT_VAULT =
-  "/Users/syang/Library/CloudStorage/GoogleDrive-syyangv@gmail.com/我的云端硬盘/obsidian_vaults/syang";
+  "/Users/syang/obsidian/syang";
 
 const pluginId = "obsidian-progress-frontmatter";
 const vaultPath = process.env.OBSIDIAN_VAULT || DEFAULT_VAULT;
